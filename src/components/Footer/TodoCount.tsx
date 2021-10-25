8f85dbd351e435d67dd67019dc0dd4b68c935009
Dami Kim <dami.k.dev@gmail.com>
2021-10-26 00:01:39 +0900

    feat: left todoItem count 기능 추가

--- src/components/Footer/TodoCount.tsx
+++ src/components/Footer/TodoCount.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { observer } from 'mobx-react';
+import useStore from '../../hooks/useStore';
 
 const TodoCount = () => {
+  const { todoStore } = useStore();
+
+  const count = todoStore.getTodoList().filter((todo) => !todo.isCompleted).length;
+
   return (
     <span className="todo-count">
-      <strong>0</strong> item left
+      <strong>{count}</strong> item left
     </span>
   );
 };
 
-export default TodoCount;
+export default observer(TodoCount);
